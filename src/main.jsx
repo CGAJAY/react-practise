910550f0a9f50276267cf0d56ba5eb7f1e19f334
CGAJAY <jmugendi737@gmail.com>
2024-09-03 18:04:32 +0300

    Created the Navbar and added the logo file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Counter from "./app.jsx";
+import App from "./app.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Counter />
+		<App />
 	</StrictMode>
 );
