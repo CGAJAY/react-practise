5d3154afa67639d9684f79ba2e185c483d67a5b4
CGAJAY <jmugendi737@gmail.com>
2024-09-03 19:50:37 +0300

    Added the hero section

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
+	const pageTitle = `Become a React Dev`;
+	const pageSubtitle = `Find the React job that fits your skills and
+						needs`;
 	return (
 		<>
 			<Navbar />
+			<Hero title={pageTitle} subtitle={pageSubtitle} />
 		</>
 	);
 };
